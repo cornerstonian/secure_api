da702b140eeac6cd5a8b6220539fbfb5497c5df6
cornerstonian <vois.cornerstone@gmail.com>
2016-07-10 03:07:59 -0500

    api controllers, json views, routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+
+  use_doorkeeper
   namespace :api do
     get 'posts' => 'posts#index'
     get 'posts/:id' => 'posts#show'
+    post 'registrations' => 'registrations#create'
   end
 end
