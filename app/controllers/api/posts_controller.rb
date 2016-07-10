f0702b5876bba0d6359b3dac57d9f1618fb15499
cornerstonian <vois.cornerstone@gmail.com>
2016-07-10 18:17:36 -0500

    add CRUD for posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,13 +1,42 @@
 class Api::PostsController < ApplicationController
 
   before_action :doorkeeper_authorize!
+  protect_from_forgery with: :null_session
 
   def index
     @posts = Post.all
   end
+  
 
   def show
     @post = Post.find_by id: params[:id]
+  end
+
+
+  def create
+    @post = Post.new params.require(:post).permit(:title)
+    if @post.save
+      render :show
+    else
+      render json: @upost.errors, status: 422
+    end
+  end
+
+
+  def update
+    @post = Post.find_by id: params[:id]
+    if @post.update params.require(:post).permit(:title)
+      render :show
+    else
+      render json: @upost.errors, status: 422
+    end
+  end
+
+
+  def delete
+    @post = Post.find_by id: params[:id]
+    @post.destroy
+    head :ok
   end
 
 
